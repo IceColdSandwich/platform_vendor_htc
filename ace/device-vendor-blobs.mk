7bf1307e210efd761b3d6d0a072937d391556b8d
LorD ClockaN <davor@losinj.com>
2012-03-30 20:50:08 +0200

    don't use prebuilt GooManager

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -42,5 +42,4 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/apps/wifixmanager.apk:/system/app/wifixmanager.apk \
     vendor/htc/ace/proprietary/libbcc.so:/system/lib/libbcc.so \
     vendor/htc/ace/apps/modelid_cfg.sh:/system/bin/modelid_cfg.sh \
-    vendor/htc/ace/apps/GooManager_1.5.7_signed.apk:/system/app/GooManager.apk \
     vendor/htc/ace/apps/NovaLauncher_1.0.2.apk:/system/app/NovaLauncher.apk

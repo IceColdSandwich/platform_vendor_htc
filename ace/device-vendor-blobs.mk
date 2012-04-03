1f0417578b070283a896d0da32d52e755402653d
LorD ClockaN <davor@losinj.com>
2012-04-03 15:35:14 +0200

    update Nova to 1.0.3b1

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -42,4 +42,4 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/apps/wifixmanager.apk:/system/app/wifixmanager.apk \
     vendor/htc/ace/proprietary/libbcc.so:/system/lib/libbcc.so \
     vendor/htc/ace/apps/modelid_cfg.sh:/system/bin/modelid_cfg.sh \
-    vendor/htc/ace/apps/NovaLauncher_1.0.2.apk:/system/app/NovaLauncher.apk
+    vendor/htc/ace/apps/NovaLauncher_1.0.3beta1.apk:/system/app/NovaLauncher.apk

1b8b455bcb02ccea6b73fef6aecdddb076cf970f
LorD ClockaN <davor@losinj.com>
2012-04-27 15:37:02 +0200

    Update ApexLauncher to 1.1.0 final

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -43,4 +43,4 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/libbcc.so:/system/lib/libbcc.so \
     vendor/htc/ace/apps/modelid_cfg.sh:/system/bin/modelid_cfg.sh \
     vendor/htc/ace/apps/NovaLauncher_1.1.2.apk:/system/app/NovaLauncher.apk \
-    vendor/htc/ace/apps/ApexLauncher_v1.1.0beta4.apk:/system/app/Apexbeta.apk
+    vendor/htc/ace/apps/ApexLauncher_v1.1.0.apk:/system/app/ApexLauncher.apk

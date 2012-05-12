53444d60a901eb053599ac045eab40c893e25f50
LorD ClockaN <davor@losinj.com>
2012-05-12 20:31:38 +0200

    Update Apex to 1.2.obeta1

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -43,4 +43,4 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/libbcc.so:/system/lib/libbcc.so \
     vendor/htc/ace/apps/modelid_cfg.sh:/system/bin/modelid_cfg.sh \
     vendor/htc/ace/apps/NovaLauncher_1.1.3beta3.apk:/system/app/NovaLauncher.apk \
-    vendor/htc/ace/apps/ApexLauncher_v1.1.1.apk:/system/app/ApexLauncher.apk
+    vendor/htc/ace/apps/ApexLauncher_v1.2.0beta1.apk:/system/app/ApexLauncher.apk

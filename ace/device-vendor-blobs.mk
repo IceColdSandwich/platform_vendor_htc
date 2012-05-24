71813f6317f0fb7d81afebc65249d503ed47895b
LorD ClockaN <lord@mint-PC.(none)>
2012-05-24 21:37:34 +0200

    update Launchers

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -42,5 +42,5 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/apps/wifixmanager.apk:/system/app/wifixmanager.apk \
     vendor/htc/ace/proprietary/libbcc.so:/system/lib/libbcc.so \
     vendor/htc/ace/apps/modelid_cfg.sh:/system/bin/modelid_cfg.sh \
-    vendor/htc/ace/apps/NovaLauncher_1.1.3.apk:/system/app/NovaLauncher.apk \
-    vendor/htc/ace/apps/ApexLauncher_v1.2.0beta2.apk:/system/app/ApexLauncher.apk
+    vendor/htc/ace/apps/NovaLauncher_1.1.4.apk:/system/app/NovaLauncher.apk \
+    vendor/htc/ace/apps/ApexLauncher_v1.2.1.apk:/system/app/ApexLauncher.apk

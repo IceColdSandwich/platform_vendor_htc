1b92630ae3809c22e59bf9e16c585fd1239bbbc4
LorD ClockaN <davor@losinj.com>
2012-06-23 20:55:50 +0200

    add prebuilt Phone.apk that is all-DPI ready by LorD ClockaN

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -42,4 +42,5 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/apps/wifixmanager.apk:/system/app/wifixmanager.apk \
     vendor/htc/ace/apps/modelid_cfg.sh:/system/bin/modelid_cfg.sh \
     vendor/htc/ace/apps/NovaLauncher_1.2.1.apk:/system/app/NovaLauncher.apk \
-    vendor/htc/ace/apps/ApexLauncher_v1.2.2.apk:/system/app/ApexLauncher.apk
+    vendor/htc/ace/apps/ApexLauncher_v1.2.2.apk:/system/app/ApexLauncher.apk \
+    vendor/htc/ace/apps/Phone.apk:/system/app/Phone.apk

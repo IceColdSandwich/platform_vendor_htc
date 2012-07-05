9898b2991f77d9fa04305f80c5ddd0620018ebf5
LorD ClockaN <davor@losinj.com>
2012-07-05 17:55:07 +0200

    Update Nova and Apex

--- ace/device-vendor-blobs.mk
+++ ace/device-vendor-blobs.mk
@@ -41,7 +41,7 @@ PRODUCT_COPY_FILES += \
     vendor/htc/ace/proprietary/gps.spade.so:/system/lib/hw/gps.spade.so \
     vendor/htc/ace/apps/wifixmanager.apk:/system/app/wifixmanager.apk \
     vendor/htc/ace/apps/modelid_cfg.sh:/system/bin/modelid_cfg.sh \
-    vendor/htc/ace/apps/NovaLauncher_1.2.1.apk:/system/app/NovaLauncher.apk \
-    vendor/htc/ace/apps/ApexLauncher_v1.2.3.apk:/system/app/ApexLauncher.apk \
+    vendor/htc/ace/apps/NovaLauncher_1.2.2beta2.apk:/system/app/NovaLauncher.apk \
+    vendor/htc/ace/apps/ApexLauncher_v1.2.4.apk:/system/app/ApexLauncher.apk \
     vendor/htc/ace/apps/Phone.apk:/system/app/Phone.apk \
     vendor/htc/ace/apps/QuickSearchBox.apk:/system/app/QuickSearchBox.apk
